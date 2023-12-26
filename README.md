2fa14e2b9ae6cc6f9f0830e78666e583c4f02b14
Zach Molony <31472522+zachmolony@users.noreply.github.com>
2023-12-26 17:16:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Big Smoke Corp Project
+
+This project was bootstrapped with [`create-r3f-app`](https://github.com/utsuboco/create-r3f-app)
---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import { Layout } from '@/components/dom/Layout'
+import '@/global.css'
+
+export const metadata = {
+  title: 'Big Smoke Corporation',
+  description: 'Exclusive management for Skepta.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en' className='antialiased'>
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
+        <Layout>{children}</Layout>
+      </body>
+    </html>
+  )
+}
